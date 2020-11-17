1174edcbfdb84d8a2de80a2f993c92d8c69c79ec
Gitai <gitai@qq.com>
2020-11-17 20:15:04 +0800

    rebuilding site 2020-11-17 20:15:03

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,20 @@
 # If a command fails then the deploy stops
 set -e
 
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+git add .
+
+git commit -m "$msg"
+
+git push origin master:master
+
+hugo
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
@@ -18,11 +32,6 @@ cd public
 # Add changes to git.
 git add .
 
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
 git commit -m "$msg"
 
 # Push source and build repos.
